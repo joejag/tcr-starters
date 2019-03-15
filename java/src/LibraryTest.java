bb8534a6bfef863330eb8bfe89b8145ee5c62c73
Joe Wright <joe@joejag.com>
2019-03-15 22:42:33 +0000

    working

--- java/src/LibraryTest.java
+++ java/src/LibraryTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        classUnderTest.someLibraryMethod();
+        classUnderTest.someLibraryMethod22();
 
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
