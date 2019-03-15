1b6de44d339c8a433abaeb0c925db1c0c9a04161
Joe Wright <joseph.wright@ros.gov.uk>
2019-03-15 16:14:38 +0000

    Move things around

--- java/run.sh
+++ java/run.sh
@@ -1,3 +1,9 @@
-rm -rf *.class
-javac -cp lib/hamcrest-all-1.3.jar:lib/junit-4.13-beta-2.jar *.java 
-java -cp .:lib/hamcrest-all-1.3.jar:lib/junit-4.13-beta-2.jar org.junit.runner.JUnitCore LibraryTest
+#!/usr/bin/env bash
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+rm -rf build/*.class
+javac -cp lib/hamcrest-all-1.3.jar:lib/junit-4.13-beta-2.jar src/*.java -d build
+java -cp build:lib/hamcrest-all-1.3.jar:lib/junit-4.13-beta-2.jar org.junit.runner.JUnitCore LibraryTest
