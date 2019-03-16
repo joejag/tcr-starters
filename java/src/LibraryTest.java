6a97f33ce0a77c242b024c860ce3337b89023f6e
Joe Wright <joe@joejag.com>
2019-03-16 14:09:38 +0000

    working

--- java/src/LibraryTest.java
+++ java/src/LibraryTest.java
@@ -5,6 +5,7 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
 
+        
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
