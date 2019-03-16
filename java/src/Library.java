3381445e06fa699688830a5388c1d179c4d057b2
Joe Wright <joe@joejag.com>
2019-03-16 22:09:12 +0000

    working

--- java/src/Library.java
+++ java/src/Library.java
@@ -1,8 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Library {
     public boolean someLibraryMethod() {
-
-
-        
         return true;
     }
+
+    public class Library2Test {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
 }
