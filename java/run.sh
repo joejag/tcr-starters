eb1774cd4d9f9092309744f7c6205241f2dd318b
Joe Wright <joe@joejag.com>
2019-03-16 22:37:28 +0000

    Move into package

--- java/run.sh
+++ java/run.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-mkdir -p build
 rm -rf build/*.class
+mkdir -p build
 javac -cp lib/junit-platform-console-standalone-1.4.0.jar src/tcr/*.java -d build
 java -jar lib/junit-platform-console-standalone-1.4.0.jar --class-path build --scan-class-path --disable-banner --details=none
 echo "All tests passed!"
