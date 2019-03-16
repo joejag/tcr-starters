e58af056a5882843af298e59192bf5ecd83f0236
Joe Wright <joe@joejag.com>
2019-03-16 14:10:28 +0000

    working

---
+++ java/src/Library2Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Library2Test {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+
+        
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
