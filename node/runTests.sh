ef11a7857ea9e9433664a1af5deb4fa34469cb69
Joe Wright <joe@joejag.com>
2019-03-17 15:59:30 +0000

    shebang

--- node/runTests.sh
+++ node/runTests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m unittest discover . *.py
+npx jest
--- ruby/runTests.sh
+++ ruby/runTests.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 ruby -I . -e "require 'minitest/autorun'; ARGV << '--color'; Dir.glob('**/*.rb') { |f| require(f) }"
