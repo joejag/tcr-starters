f3c8c531bcb688f3db5a3af5b2aa4a02c3174df0
Joe Wright <joe@joejag.com>
2019-03-18 22:40:45 +0000

    Update runTests.sh

--- ruby/runTests.sh
+++ ruby/runTests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ruby -I . -e "require 'minitest/autorun'; ARGV << '--color'; Dir.glob('**/*.rb') { |f| require(f) }"
+ruby -I . -e "require 'minitest/autorun'; Dir.glob('**/*.rb') { |f| require(f) }"
